34b1e6da6f76d9f180049c8127ab9db3e20f459b
Nithish-maker <nithishraina0323@gmail.com>
2021-03-22 00:23:09 +0530

    Day 2

--- basic-algos/new.js
+++ basic-algos/new.js
@@ -88,3 +88,109 @@ const getCount = () => {
   return count;
 }
 console.log(getCount());
+
+//maximum char count 
+var str = prompt("Enter string");
+let result = {};
+const maximumChar = string => {
+  for(let char of str) {
+    result[char] = result[char] + 1 || 1;
+  }
+  let maxCount = 0;
+  let maxChar = null;
+  for(let ch in result) {
+    if(result[ch] > maxCount) {
+      maxCount = result[ch];
+      maxChar = ch;
+    }
+  }
+  return maxChar;
+}
+console.log(maximumChar(str));
+
+//reversing an array 
+
+let arr = [1,2,3,4,5];
+const reverse = array => {
+	for(let i = 0; i < array.length / 2; i++) {
+		let temp = array[i];
+		array[i] = array[array.length - 1 - i];
+		array[array.length - 1 - i] = temp;
+	}
+	return arr;
+}
+console.log(reverse(arr));
+
+//reversing an array easy peasy
+let arr = [1,2,3,4,5];
+const reversing = array => {
+	return array.reverse();
+}
+
+//word reversal 
+const wordRev = str => {
+	return str.split(" ").reverse().join(" ");
+}
+console.log(wordRev("Sentence to be reversed"));
+
+//capitalizing sentences 
+let str = prompt("Enter a string");
+const capitalizeWord = string => {
+ return string[0].toUpperCase() + string.slice(1,string.length);
+}
+const capitalize = string => {
+  return string.split(" ").map((iter) => capitalizeWord(iter)).join(" ")
+}
+
+console.log(capitalize(str));
+
+//mean of an array 
+let arr = [1,2,3,4,5];
+let sum = 0;
+const mean = array => {
+	for(let i = 0; i < array.length; i++) {
+		sum+=array[i];
+	}
+	return (sum / (array.length));
+}
+console.log(mean(arr));
+
+//median of an array 
+let arr = [1,2,3,4,5,6,7];
+const median = array => {
+	let med = 0;
+	let medEvenIndex = Math.round(array.length / 2);
+	let medOddIndex = Math.round((array.length / 2) - 1);
+	if(array.length % 2 === 0) {
+		med = array[medEvenIndex] + (array[medEvenIndex] - 1);
+	}
+	else if(array.length % 2 != 0) {
+		med = array[medOddIndex];
+	}
+	return med;
+}
+console.log(median(arr));
+
+//mode of an  array
+
+let arr = [1,2,3,3,4,5,5,6,6,7,7,7,7];
+const mode = num => {
+  let obj = {};
+  let modes = [];
+  for(let num of arr) {
+    obj[num] = obj[num] + 1 || 1;
+  }
+  let max = 0;
+  for(let i in obj) {
+    let count = obj[i];
+    if(count > max) {
+      max = count;
+      modes = [i];
+    }
+    else if(count == max) {
+      modes.push(i);
+    }
+  }
+  return modes;
+}
+console.log(mode(arr));
