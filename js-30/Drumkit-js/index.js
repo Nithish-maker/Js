7fe51b3bd79114345e77a2123ea8391e20502e62
Nithish-maker <nithishraina0323@gmail.com>
2021-04-02 21:19:08 +0530

    Day 5 and 6

---
+++ js-30/Drumkit-js/index.js
@@ -0,0 +1,32 @@
+//adding event listener for the whole window along with the keydown event listener
+// const logValues = (e) => {
+// 	console.log(e.keyCode);
+// }
+const removeTransition = (e) => {
+	let isClassPresent = (e.propertyName === 'transform') ? true : false;
+	if(isClassPresent) {
+		e.target.classList.remove('play');
+	}
+ }
+//looping through each key element and adding a function callback
+//getting the whole array of keys using required DOM selection 
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.map(key => key.addEventListener('transitionend', removeTransition));
+
+const playSound = (e) => {
+	//getting the pressed key element respectively
+	const resKey = document.querySelector(`div[data-key='${e.keyCode}']`);
+	//getting respective audio element using attribute selection and adding class play and playing the respective audio ele sound
+	const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+	if(audio === null) {
+		alert('Press a valid key from [asdfghjkl]');
+	}
+	//console.log(audio);
+	//adding the class play to the specific keyelement being pressed
+	resKey.classList.add('play');
+	//reinitalising the value of currenttime to 0 inorder to play the audio again when the key is pressed again
+	//setting the current value of the audio element as 0
+	audio.currentTime = 0;
+	audio.play();
+}
+window.addEventListener('keydown',playSound);
---
+++ js-30/clock/index.js
@@ -0,0 +1,43 @@
+ 
+//for each second getting the time units such as hours seconds and hours with the supported js data and time function
+console.log("js linked properly");
+
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate(){
+		const now=new Date();
+		const seconds= now.getSeconds();
+		const minutes=now.getMinutes();
+		const hours=now.getHours();
+		const secondsDegrees = ((seconds/60)*360)+90;
+		const minutesDegrees = ((minutes/60)*360)+90;
+		const hoursDegrees = ((hours/12)*360)+90;
+		secondHand.style.transform=`rotate(${secondsDegrees}deg)`;
+		if(seconds === 0) secondHand.classList.add('fast');
+			console.log("added");
+		if(seconds === 1) secondHand.classList.remove('fast');
+			console.log("removed");
+
+		minuteHand.style.transform=`rotate(${minutesDegrees}deg)`;
+		if(minutes === 0) {
+			minuteHand.classList.add('fast');
+			console.log("added");
+		}
+		if(minutes === 1) {
+			minuteHand.classList.remove('fast');
+			console.log("removed");
+		}
+
+		hourHand.style.transform=`rotate(${hoursDegrees}deg)`;
+		if(hours === 0) {
+			hourHand.classList.add('fast');
+			console.log("added");
+		}
+		if(hours === 1) {
+			hourHand.classList.remove('fast');
+			console.log("removed");
+		}
+	}
+	setInterval(setDate, 1000);
