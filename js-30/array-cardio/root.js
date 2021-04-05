b262a844ea99c581f98882482c1372e93c0fdbc1
Nithish-maker <nithishraina0323@gmail.com>
2021-04-05 18:37:04 +0530

    Day 4

--- js-30/array-cardio/root.js
+++ js-30/array-cardio/root.js
@@ -20,42 +20,43 @@ const people = [
       'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
     ];
 
-//1. Filter the list of inventors for those who were born in the 1500's
-// // with higher order functions 
-// const filtered = inventors.filter(inventor => 1499 < inventor.year && 1600 > inventor.year);
-// console.table(filtered);
+// 1. Filter the list of inventors for those who were born in the 1500's
+// with higher order functions 
 
-// // Without higher order function
-// function filtering(arr) {
-// 	let newArr = [];
-// 	for(let i of arr) {
-// 		if(1499 < i['year'] && 1600 > i['year']) {
-// 			newArr.push(i);
-// 		}
-// 	}
-// 	return newArr;
-// }
-// console.table(filtering(inventors));
+const filtered = inventors.filter(inventor => 1499 < inventor.year && 1600 > inventor.year);
+console.table(filtered);
+ 
+// Without higher order function
 
+function filtering(arr) {
+	let newArr = [];
+	for(let i of arr) {
+		if(1499 < i['year'] && 1600 > i['year']) {
+			newArr.push(i);
+		}
+	}
+	return newArr;
+}
+console.table(filtering(inventors));
 
 // 2. Give us an array of the inventor first and last names
 
 // With higher order function
 
-// const arrOfNames = inventors.map(name => (name.first + " " + name.last));
-// console.table(arrOfNames); 
+const arrOfNames = inventors.map(name => (name.first + " " + name.last));
+console.table(arrOfNames); 
 
 // Without higher order function 
 
-// const arrOfNames = (arr) => {
-// 	let resArr = [];
-// 	for(let it = 0; it < arr.length; it++) {
-// 		let resName = arr[it]['first'] + " " + arr[it]['last'];
-// 		resArr.push(resName);
-// 	}
-// 	return resArr;
-// }
-// console.log(arrOfNames(inventors));
+const arrNames = (arr) => {
+	let resArr = [];
+	for(let it = 0; it < arr.length; it++) {
+		let resName = arr[it]['first'] + " " + arr[it]['last'];
+		resArr.push(resName);
+	}
+	return resArr;
+}
+console.log(arrNames(inventors));
 
 // 3. Sort the inventors by birthdate, oldest to youngest
 
@@ -68,29 +69,55 @@ const people = [
 **/
 
 
-// console.table(inventors);
+console.table(inventors);
 
-// const sortedArr = inventors.sort((firstIt,nextIt) => (firstIt.year - nextIt.year));
-// console.table(sortedArr);
+const sortedArr = inventors.sort((firstIt,nextIt) => (firstIt.year - nextIt.year));
+console.table(sortedArr);
 
 // 4. How many years did all the inventors live?
 
 // With higher order function 
 
-// const livedYears = inventors.reduce((total,inventor) => {
-// 	return total + (inventor.passed - inventor.year);
-// },0)
+const livedYears = inventors.reduce((total,inventor) => {
+	return total + (inventor.passed - inventor.year);
+},0)
 // console.log(livedYears);
 
 // //Without higher order function
 
-// const getLivedYears = (arr) => {
-// 	let total = 0; 
-// 	for(let it of arr) {
-// 		total+=it['passed'] - it['year'];
-// 	}
-// 	return total;
-// }
-// console.log(getLivedYears(inventors));
+const getLivedYears = (arr) => {
+	let total = 0; 
+	for(let it of arr) {
+		total+=it['passed'] - it['year'];
+	}
+	return total;
+}
+console.log(getLivedYears(inventors));
 
+// 5. Sort the inventors by years lived
 
+const liveSort = inventors.sort((firstPerson,nextPerson) => (firstPerson.passed - firstPerson.year) - (nextPerson.passed - nextPerson.year));
+console.table(liveSort);
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+// const container = document.querySelector('.mw-category');
+// const links = Array.from(container.querySelectorAll('a'));
+// const res = links.map(link => link.title);
+// const fRes = res.filter(item => item.includes('de'));
+// console.log(fRes);
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'pogostick'];
+
+const res = data.reduce(function(obj,item) {
+    if(!obj[item]) {
+      obj[item] = 0;
+    }
+    obj[item] = obj[item] + 1;
+    return obj;
+   },{});
+console.table(res);
