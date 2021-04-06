dd1aa4440caba6d5d9b27d06121f4af395e0fe77
Nithish-maker <nithishraina0323@gmail.com>
2021-04-06 17:14:59 +0530

    Day 8

---
+++ js-30/flexbox-gallery/root.js
@@ -0,0 +1,16 @@
+ const getPanel = document.querySelectorAll('.panel');
+
+const flexGrow = (e) => {
+	let arr = Array.from(e.target.classList);
+	if(arr.some(item => item === 'panel')) {
+		e.target.classList.add('panel-grow');	
+	}
+}
+const textTransition = (e) => {
+	// console.log(e.propertyName);
+	if (e.propertyName.includes('flex')) {
+        e.classList.add('text-shift');
+      }
+}
+getPanel.forEach(panel => panel.addEventListener('click',flexGrow));
+getPanel.forEach(panel => panel.addEventListener('transitioned',textTransition));
