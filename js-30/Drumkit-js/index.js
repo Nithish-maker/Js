31171be78f1ece57570a9707b91132f75918de61
Nithish-maker <nithishraina0323@gmail.com>
2021-05-03 12:21:18 +0530

    bug fixes

--- js-30/Drumkit-js/index.js
+++ js-30/Drumkit-js/index.js
@@ -15,15 +15,15 @@ keys.map(key => key.addEventListener('transitionend', removeTransition));
 
 const playSound = (e) => {
 	//getting the pressed key element respectively
-	const resKey = document.querySelector(`div[data-key='${e.target.keyCode}']`);
+    const reskey = document.querySelector(`div[data-key="${e.keyCode}"]`);
 	//getting respective audio element using attribute selection and adding class play and playing the respective audio ele sound
-	const audio = document.querySelector(`audio[data-key='${e.target.keyCode}']`);
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	if(audio === null) {
 		alert('Press a valid key from [asdfghjkl]');
 	}
 	//console.log(audio);
 	//adding the class play to the specific keyelement being pressed
-	resKey.classList.add('play');
+	reskey.classList.add('play');
 	//reinitalising the value of currenttime to 0 inorder to play the audio again when the key is pressed again
 	//setting the current value of the audio element as 0
 	audio.currentTime = 0;
